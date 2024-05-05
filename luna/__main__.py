3dda9ed0605b4c77fd45c585ef0413b73afe7c78
Dirk Doesburg <dirk.doesburg@gmail.com>
2024-05-05 10:09:33 +0200

    Add a CLI

---
+++ luna/__main__.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import click
+
+from .training import Trainer
+
+
+@click.command()
+@click.option("--fold", default=0)
+@click.option(
+    "--exp-id",
+    type=str,
+    help="A name for this experiment. Paths will always get timestamps as well.",
+)
+@click.option("--epochs", default=100)
+@click.option(
+    "--data-dir",
+    envvar="DATA_DIR",
+    required=True,
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+)
+def train(data_dir: str, epochs=100, exp_id: str | None = None, fold=0):
+    trainer = Trainer(Path(data_dir), fold=fold, epochs=epochs)
+    trainer.train()
+
+
+@click.group()
+def cli():
+    pass
+
+
+cli.add_command(train)
+
+if __name__ == "__main__":
+    cli()
