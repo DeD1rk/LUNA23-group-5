ff115427891d9999b8a77a78fa50a040a6afe603
Cluster <dirk.doesburg@gmail.com>
2024-05-19 12:46:16 +0200

    Check in some small changes

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --time=03:00:00
+#SBATCH --time=02:30:00
 #SBATCH --mem=32G
 #SBATCH --output=/home/danne/slurm_output/%j.out
 
@@ -27,13 +27,17 @@ python3 -m pip install --user -r $HOME/LUNA23-group-5/requirements.txt
 python -u -m luna train \
     --data-dir=$TMPDIR/dataset \
     --results-dir=$TMPDIR/results \
-    --batch-size=8 \
-    --epochs=100 \
-    --fold=1 \
-    --segmentation-weight=4.0 \
+    --batch-size=16 \
+    --epochs=300 \
+    --fold=0 \
+    --segmentation-weight=2.0 \
     --noduletype-weight=1.0 \
-    --malignancy-weight=2.0 \
-    --exp-id="w411" \
+    --malignancy-weight=1.0 \
+    --aug-mirror-x \
+    --aug-mirror-y \
+    --aug-mirror-z \
+    --dropout=0.2 \
+    --exp-id="baseline-b16-e300-w211-aug-xyz-d02-lr1e-4" \
     --perform-inference
 
 cp -r $TMPDIR/results/* $HOME/results/
